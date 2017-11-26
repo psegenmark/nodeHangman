4f2e7d2c8a0e192eb88a70b4ad758157c5e7bf69
psegenmark <psegenmark@gmail.com>
2017-11-25 19:03:19 -0800

    done

---
+++ main.js
@@ -0,0 +1,68 @@
+var inquirer = require("inquirer");
+var wordsAvailable = require("./word.js");
+
+var randomWord = wordsAvailable[Math.floor(Math.random() * 11) + 1];
+
+function Word(word, length) {
+    this.word = word;
+    this.length = length;
+}
+
+function Guess(guess) {
+    this.guess = guess;
+}
+
+        var chosenWord = new Word(randomWord, randomWord.length);
+        var chosenWordArr = chosenWord.word.split('');
+    
+        var guesses = [];
+        var wordBuild = [];
+        var guessesLeft = 0;
+
+        for (let i=0;chosenWord.length>i;i++) {
+            wordBuild.push(" _ ");
+        };
+        
+        function runGame(){
+
+        if (wordBuild.join("") === chosenWord.word) {
+            console.log("Congratulations! You have defeated me");
+        }
+        else {
+
+        inquirer.prompt([
+            {
+              name: "guess",
+              message: "Guess a Letter!"
+            }]).then(function(answers) {
+                var guess = new Guess(answers);
+
+            for (let i=0;chosenWordArr.length>i;i++) {
+                if(guess.guess.guess === chosenWordArr[i]) {
+                    wordBuild[i] = guess.guess.guess;
+                    console.log("CORRECT!")
+                }
+            }
+            console.log("Word to Guess " + wordBuild.join(""));
+            console.log("Guesses Left:" + (chosenWord.length-guessesLeft));
+        
+
+        if (guessesLeft<chosenWord.length) {
+            
+            guessesLeft++;
+            guesses.push(guess.guess.guess);
+            console.log("Guessed Letters " + guesses);
+            runGame();
+            
+        }
+        else {
+            console.log("Game Over");
+            console.log("Word was " + chosenWord.word);
+        
+        };
+        
+    });
+    }    
+    };
+    runGame();
+
---
+++ word.js
@@ -0,0 +1,4 @@
+var wordsAvailable = ["coffee", "melrose place", "thanksgiving", "tequila", "beaujolais", 
+                      "javascript", "apple", "bootcamp", "parking", "mash", "palms", "tevva"];
+
+module.exports = wordsAvailable;
